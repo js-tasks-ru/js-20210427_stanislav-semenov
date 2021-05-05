8ee7cbccc612b62f9920a91f677452a9bc803dd2
Stanislav Semenov <stanislav.semenov2386@gmail.com>
2021-05-05 22:03:09 +0300

    3.Объекты и массивы, основы тестирования. Задания на проверку: 1,2,4

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,39 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
+function isEmpty(obj) {
+
+    if (obj === undefined) {
+        console.error('no Object ')
+        return true
+    }
+    else {
+        for (let key in obj) {
+            return false;
+        }
+        console.error('Object is empty')
+        return true
+    }
+}
+
 export function createGetter(path) {
 
+    if (path === undefined) {
+        console.error('no Path')
+        return {}
+    }
+    else
+        return (obj) => {
+            let value
+            if (!isEmpty(obj)) {
+
+                value = { ...obj }
+
+                for (const key of path.split('.')) {
+                    if (!isEmpty(value))
+                        value = value[key]
+                }
+            }
+            return value
+        }
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -5,4 +5,16 @@
  */
 export function invertObj(obj) {
 
+    if (obj === undefined)
+        return undefined
+    else {
+        const newObj = {}
+
+        for (const [key, value] of Object.entries({ ...obj })) {
+            if (typeof value === 'object')
+                return console.error('object properties cannot be objects')
+            else newObj[value] = key
+        }
+        return newObj
+    }
 }
