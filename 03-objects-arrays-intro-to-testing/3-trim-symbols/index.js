12f9d8fc22cc0d1efe342905e9170aacc1c0b4be
Stanislav Semenov <stanislav.semenov2386@gmail.com>
2021-05-07 02:04:09 +0300

    Объекты и массивы, основы тестирования. trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,38 @@
  */
 export function trimSymbols(string, size) {
 
+    if (typeof size !== 'number') {
+        console.error('The second argument(size) is not a number')
+        return string
+    }
+
+    if (typeof size === undefined) {
+        console.error('The second argument(size) is not defined')
+        return string
+    }
+
+    if (typeof string === undefined) {
+        console.error('The string is not defined')
+        return
+    }
+
+    let arr = string.split('')
+    let newStr = ''
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === arr[i + 1])
+            (i < size) && (newStr += arr[i])
+
+        else {
+            (i < size) && (newStr += arr[i])
+            newArr.push(newStr)
+            arr = arr.splice(i + 1, arr.length + 1)
+            i = -1
+            newStr = ''
+        }
+    }
+
+    return newArr.join('')
 }
