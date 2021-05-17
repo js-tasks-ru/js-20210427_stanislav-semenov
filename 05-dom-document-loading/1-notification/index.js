8010153720cb0cd0f7c662c15f4f0ee2b4448e13
Stanislav Semenov <stanislav.semenov2386@gmail.com>
2021-05-17 22:13:38 +0300

    1 задание с ошибкой теста  received value must be an HTMLElement or an SVGElement.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
+    static displayNotification;
 
+    constructor(
+        string = '',
+        {
+            duration = 0,
+            type = ''
+        } = {}) {
+
+        this.string = string;
+        this.duration = duration;
+        this.type = type;
+    }
+
+    show(sub = document.body) {
+        if (NotificationMessage.displayNotification) {
+            NotificationMessage.displayNotification.remove();
+        }
+        sub.append(this.render());
+        setTimeout(() => this.destroy(), this.duration);
+    }
+
+    get template() {
+        return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.string}
+          </div>
+        </div>
+      </div>`;
+    }
+
+    render() {
+        this.element = document.createElement('div');
+        this.element.innerHTML = this.template;
+        NotificationMessage.displayNotification = this.element;
+        console.log(this.element.innerHTML)
+        return NotificationMessage.displayNotification;
+    }
+    remove() {
+        if (this.element) this.element.remove();
+        this.element = null
+    }
+    destroy() {
+        this.remove();
+    }
 }
