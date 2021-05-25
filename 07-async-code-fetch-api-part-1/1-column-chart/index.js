0f7d7d64d0f4d9946f8cd3a6083edf3af15dbee1
Stanislav Semenov <stanislav.semenov2386@gmail.com>
2021-05-25 17:30:57 +0300

    не работает с this.data=[]

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -10,43 +10,29 @@ export default class ColumnChart {
 
     constructor({
 
-        url = 'api/dashboard/customers',
+        url = '',
         range = {
-            from: '2020-04-06',
-            to: '2020-05-06',
+            from: new Date(),
+            to: new Date(),
         },
         label = 'orders',
         formatHeading = data => data
     } = {}) {
         this.url = url;
         this.range = range;
-        this.data = [1, 2, 3, 4, 5];
+        this.data = [];
         this.label = label;
-        this.value = 15;
+        this.value = 0;
         this.link = '';
         this.formatHeading = formatHeading;
 
         this.getDataServer(this.range.from, this.range.to);
         this.render();
-        const getData = (data) => {
-            let itemData = [];
-            for (let item of Object.values(data)) {
-                itemData.push(item);
-                this.value += item;
-            }
-            this.data = itemData;
-            this.render();
-
-        }
     }
 
-
-
     getColumnBody(data) {
-
         const maxValue = Math.max(...data);
         const scale = this.chartHeight / maxValue;
-
         return data
             .map(item => {
                 const percent = (item / maxValue * 100).toFixed(0);
@@ -78,20 +64,21 @@ export default class ColumnChart {
         </div>`
 
     }
+    getData(data) {
+        this.data = Object.values(data);
+        this.value = this.data.reduce((total, item) => total + item);
+        this.show();
+    }
 
     getDataServer(dataFrom, dataTo) {
-        new Promise((getData) => {
-            return fetchJson(`https://course-js.javascript.ru/api/dashboard/${this.label}?
-            from=${dataFrom}&to=${dataTo}`)
-                .then(response => {
-                    getData(response);
-                    console.log(`${this.label}`, response)
-                })
-        })
+
+        fetchJson(`https://course-js.javascript.ru/api/dashboard/${this.label}?from=${dataFrom.toISOString()}&to=${dataTo.toISOString()}`)
+            .then(response => {
+                this.getData(response);
+            })
     }
 
     render() {
-
         const element = document.createElement('div');
 
         element.innerHTML = this.template;
@@ -119,13 +106,19 @@ export default class ColumnChart {
     remove() {
         this.element.remove();
     }
+
     destroy() {
         this.remove();
         this.element = null;
         this.subElements = {};
     }
-    update(data) {
-        this.getDataServer(data)
-        this.subElements.body.innerHTML = this.getColumnBody(data);
+
+    update(from = this.range.from, to = this.range.to) {
+        this.getDataServer(from, to);
+    }
+
+    show() {
+        this.subElements.body.innerHTML = this.getColumnBody(this.data);
+        this.subElements.header.innerHTML = this.formatHeading(this.value);
     }
 }
